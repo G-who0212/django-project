c882f8286859e3538caaa630d60f5ee93a87c4ec
G-who0212 <81516037+G-who0212@users.noreply.github.com>
2021-08-12 10:55:50 +0900

    Update settings.py
    ca

--- lionproject/settings.py
+++ lionproject/settings.py
@@ -140,9 +140,9 @@ DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 #aws
-AWS_ACCESS_KEY_ID = 'AKIAWODBLJNGCCX4B4ME'
+AWS_ACCESS_KEY_ID = ''
 # os.environ.get('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = 'MsD8bS2CoWJd4bHBfE5iKZedqDd0d2BSlqZGMTAx'
+AWS_SECRET_ACCESS_KEY = ''
 # os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = 'hoo-likelion-django-lesson'
 AWS_S3_SIGNATURE_VERSION = 's3v4'
